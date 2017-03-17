58f1ae59bdc1577401697f3a63312c9d11b215f4
Francisco <Francisco@Drone>
2017-03-17 21:25:58 +0000

    cliente

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,5 +1,51 @@
 package model;
 
 public class Cliente {
+	private String nome,
+		   		   morada,
+		   		   email,
+		   		   telefone;
 
+	public Cliente(String nome, String morada, String email, String telefone) {
+		super();
+		this.nome = nome;
+		this.morada = morada;
+		this.email = email;
+		this.telefone = telefone;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMorada() {
+		return morada;
+	}
+
+	public void setMorada(String morada) {
+		this.morada = morada;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+	
 }
